82c4847f7e88412c0549367bb82d78065646808c
Richard Habermann <chief@sittingbool.tech>
2024-02-06 08:50:48 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import { context } from "@actions/github";
 
 async function run() {
     try {
-        if (context.eventName !== "release") {
+        if (context.eventName !== "release" && ontext.eventName !== "push") {
             core.setFailed(`Cannot execute release action in event '${context.eventName}'.`);
             return;
         }
 
-        let tag = context.payload.release?.tag_name;
+        const event = context.payload[context.eventName] || {};
+        let tag = event.tag_name || '';
         if (tag.length < 5) {
             core.setFailed(`Invalid release tag. Expecting either 'v1.2.3' or '1.2.3' or 'v1.2.3-preview' but got '${tag}'.`);
             return;
